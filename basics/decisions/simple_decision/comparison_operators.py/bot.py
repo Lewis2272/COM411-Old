855b341ecfbafefa0b92b6ae4e0db1b6d81fe10f
Lewis227 <4rogel85@solent.ac.uk>
2020-10-25 17:21:00 +0000

    Not sure to be honest

---
+++ basics/decisions/simple_decision/comparison_operators.py/bot.py
@@ -0,0 +1,13 @@
+print("Please enter your first number")
+firstnum =int(input())
+
+print("Please enter your second number")
+secondnum = int(input())
+
+if firstnum < secondnum:
+  print("The first number is smaller than the second number")
+elif secondnum < firstnum:
+  print("The second number is smaller than the first number")
+if firstnum == secondnum:
+  print("Both of the number are equal")
+
